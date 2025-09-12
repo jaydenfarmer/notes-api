25f665d0367ca79c03b52a6c9986dc89149cb2ff
Jayden Farmer <jcfarmer420@gmail.com>
2025-09-12 19:11:37 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Notes API is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
